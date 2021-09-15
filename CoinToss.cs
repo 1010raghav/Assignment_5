cc305d306db37edbb73e0d29367f0757510c6fd4
1010raghav <89019832+1010raghav@users.noreply.github.com>
2021-09-15 23:06:11 +0530

    Coin Tossed [Head and Tails]

---
+++ CoinToss.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Assignment_5
+{
+    class CoinToss
+    {
+        public static void Coin()
+        {
+            int heads = 1;
+            Random random = new Random();
+            int toss = random.Next(0, 2);
+
+            if (toss == heads)
+                Console.WriteLine("HEADS");
+            else
+                Console.WriteLine("TAILS");
+
+
+
+        }
+
+
+
+
+
+
+
+
+
+    }
+}
